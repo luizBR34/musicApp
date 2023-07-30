abe078916431f08e6823e2b14243b3e2ebaaf420
Luiz Fernando Povoa <luizfer1@yahoo.com.br>
2023-07-29 23:21:25 -0300

    Added validation to Login and register.

--- src/main.js
+++ src/main.js
@@ -5,12 +5,14 @@ import { createPinia } from 'pinia'
 
 import App from './App.vue'
 import router from './router'
-import "./assets/base.css";
-import "./assets/main.css";
+import VeeValidatePlugin from './includes/validation'
+import './assets/base.css'
+import './assets/main.css'
 
 const app = createApp(App)
 
 app.use(createPinia())
 app.use(router)
+app.use(VeeValidatePlugin)
 
 app.mount('#app')
