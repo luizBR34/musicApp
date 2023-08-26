f0705cc600b603391a808d0cb23e2db70d78de60
Luiz Fernando Povoa <luizfer1@yahoo.com.br>
2023-08-26 20:17:59 -0300

    Refactored the code to use actions instead.

--- src/components/RegisterForm.vue
+++ src/components/RegisterForm.vue
@@ -104,7 +104,8 @@
 </template>
 
 <script>
-import { auth, usersCollection } from '@/includes/firebase'
+import { mapActions } from 'pinia'
+import useUserStore from '@/stores/user'
 
 export default {
   name: 'RegisterForm',
@@ -130,29 +131,17 @@ export default {
     }
   },
   methods: {
+    ...mapActions(useUserStore, {
+      createUser: 'register'
+    }),
     async register(values) {
       this.reg_show_alert = true
       this.reg_in_submission = true
       this.reg_alert_variant = 'bg-blue-500'
       this.reg_alert_msg = 'Please wait! Your account is being created.'
 
-      let userCred = null
       try {
-        userCred = await auth.createUserWithEmailAndPassword(values.email, values.password)
-      } catch (error) {
-        this.reg_in_submission = false
-        this.reg_alert_variant = 'bg-red-500'
-        this.reg_alert_msg = 'An unexpected error occured. Please try again later.'
-        return
-      }
-
-      try {
-        await usersCollection.add({
-          name: values.name,
-          email: values.email,
-          age: values.age,
-          country: values.country
-        })
+        await this.createUser(values)
       } catch (error) {
         this.reg_in_submission = false
         this.reg_alert_variant = 'bg-red-500'
@@ -162,7 +151,6 @@ export default {
 
       this.reg_alert_variant = 'bg-green-500'
       this.reg_alert_msg = 'Success! Your account has been created.'
-      console.log(values)
     }
   }
 }
