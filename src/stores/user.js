04dadd23c9431838620dfadfc296db1eb64c3644
Luiz Fernando Povoa <luizfer1@yahoo.com.br>
2023-08-27 19:31:05 -0300

    Added logging authentication validation.

--- src/stores/user.js
+++ src/stores/user.js
@@ -7,14 +7,23 @@ export default defineStore('user', {
   }),
   actions: {
     async register(values) {
-      await auth.createUserWithEmailAndPassword(values.email, values.password)
+      const userCred = await auth.createUserWithEmailAndPassword(values.email, values.password)
 
-      await usersCollection.add({
+      await usersCollection.doc(userCred.user.uid).set({
         name: values.name,
         email: values.email,
         age: values.age,
         country: values.country
       })
+
+      await userCred.user.updateProfile({
+        displayName: values.name
+      })
+
+      this.userLoggedIn = true
+    },
+    async authenticate(values) {
+      await auth.signInWithEmailAndPassword(values.email, values.password)
 
       this.userLoggedIn = true
     }
